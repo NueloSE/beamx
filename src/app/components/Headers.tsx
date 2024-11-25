819d0227d594ef1ff4c3d35f4acd97925da4f471
martin machiebe <martinmachiebe21@gmail.com>
2024-11-25 16:05:16 +0100

    first commit

---
+++ src/app/components/Headers.tsx
@@ -0,0 +1,21 @@
+// import { Button } from "@/components/ui/button";
+// import { ModeToggle } from "./toggle";
+
+// export default function Header() {
+//   return (
+//     <header className="w-full bg-none z-30">
+//       <div className="mx-auto m-5 flex bg-background items-center justify-around mt-4 py-4 shadow-inner px-8 rounded-full">
+//         <h1 className="text-2xl font-bold text-foreground">BeamX</h1>
+//         <div className="flex items-center justify-center gap-3">
+//           <Button
+//             variant="default"
+//             className="bg-purple-600 text-white hover:bg-purple-700"
+//           >
+//             Connect Wallet
+//           </Button>
+//           <ModeToggle />
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
