854d12a86630a05af2bde79532fa67d4b29e23b6
“Akshola00” <akinniyishola07@gmail.com>
2024-11-25 17:41:07 +0100

    feat: chat bot ui

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,39 +1,79 @@
-"use client"
-import React, { useState } from 'react';
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import logo from "../../../public/logo.webp";
+import user from "../../../public/user.png";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ weight: ["400"], subsets: ["latin"] });
 
 const ChatBot: React.FC = () => {
-  const [messages, setMessages] = useState<{ text: string; isBot: boolean }[]>([]);
-  const [inputText, setInputText] = useState('');
+  const [messages, setMessages] = useState<{ text: string; isBot: boolean }[]>(
+    []
+  );
+  const [inputText, setInputText] = useState("");
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value);
   };
 
   const handleSendMessage = () => {
-    if (inputText.trim() !== '') {
+    if (inputText.trim() !== "") {
       const newMessage = { text: inputText, isBot: false };
       setMessages((prevMessages) => [...prevMessages, newMessage]);
-      setInputText('');
+      setInputText("");
       // Call your AI model here to get the bot's response
-      const botResponse = 'Hello, I am an AI assistant.';
-      setMessages((prevMessages) => [...prevMessages, { text: botResponse, isBot: true }]);
+      const botResponse = "Hello, I am an AI assistant.";
+
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { text: botResponse, isBot: true },
+      ]);
     }
   };
 
   return (
-    <div className="flex flex-col h-screen bg-gray-800 text-white">
+    <div
+      className={`flex ${roboto.className} flex-col h-screen bg-gray-800 text-white`}
+    >
       <div className="flex-grow overflow-y-auto p-4 space-y-2">
         {messages.map((message, index) => (
           <div
             key={index}
             className={`p-2 rounded-lg ${
-              message.isBot ? 'bg-gray-700 self-start' : 'bg-blue-500 self-end'
+              message.isBot
+                ? "bg-gradient-to-r from-gray-700 to-gray-800 text-white self-start"
+                : "bg-gradient-to-r from-gray-800 to-gray-700 text-white self-end"
             }`}
           >
-            {message.text}
+            <div className="grid gap-2 justify-center  grid-cols-[32px_1fr]">
+              <div>
+                {message.isBot ? (
+                  <Image
+                    src={logo}
+                    alt="Bot Avatar"
+                    width={40}
+                    height={40}
+                    className="mr-2 rounded-full"
+                  />
+                ) : (
+                  <div className="bg-gradient-to-r from-indigo-500  p-1 rounded-full">
+                    <Image
+                      src={user}
+                      alt="Bot Avatar"
+                      width={40}
+                      height={40}
+                      className="mr-2 rounded-full"
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="text-xl leading-8">{message.text}</div>
+            </div>
           </div>
         ))}
       </div>
+
       <div className="bg-gray-700 p-4 flex space-x-2">
         <input
           type="text"
