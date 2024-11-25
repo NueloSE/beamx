bcd23ae7d792f23906c4a6dc75177d541c8a9eea
martin machiebe <martinmachiebe21@gmail.com>
2024-11-25 19:53:38 +0100

    add typewriter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ChatBot from "../components/Chat";
 import Header from "@/components/Headers";
+import TypeWriterComp from "@/components/TypeWriterComp";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <TypeWriterComp />
       <ChatBot />
     </div>
   );
